a4ac0d99c5bad94b6ca16aac0d0083970de30949
victorfernandesraton <vfbraton@gmail.com>
2020-04-24 00:32:29 -0300

    implementing ant desginer

--- src/components/auth/SinginView-container.js
+++ src/components/auth/SinginView-container.js
@@ -6,12 +6,35 @@ import {useAuth} from './Auth-context';
 
 import {singin, checkAuth} from './Auth-action';
 
+import {
+  Form,
+  Input,
+  Button,
+  Layout,
+  Row,
+  Col
+} from 'antd';
+
+const {
+  Header,
+  Content,
+  Footer,
+} = Layout;
+
 const SinginView = (props) => {
+  const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+  };
+  const tailLayout = {
+    wrapperCol: { offset: 8, span: 16 },
+  };
+
   const [username, setUsername] = useState("");
   const [password, setPasword] = useState("");
 
   const [user, setUser] = useLocalStorage('user');
-  const [state, dispatch] = useAuth()
+  const [state, dispatch] = useAuth();
   const history = useHistory();
   const location = useLocation();
   function handleSubmit(event) {
@@ -37,28 +60,44 @@ const SinginView = (props) => {
   }, [user])
   return (
     <div>
-      <h1>Login</h1>
-      {/* <form> */}
-        <label defaultValue="Login" />
-        <input
-          type="text"
-          value={username}
-          onChange={data => setUsername(data.target.value)}
-        />
-        <label defaultValue="Senha" />
-        <input
-          type="password"
-          value={password}
-          onChange={data => setPasword(data.target.value)}
-        />
-        <input
-          type="button"
-          value="Singin"
-          onClick={handleSubmit}
-          disabled={(username === "" && password === "")} />
-      {/* </form> */}
+      <Layout>
+        <Header>
+          Wather control
+        </Header>
+        <Content>
+          <Row>
+            <Col span={6} offset={8}>
+              <Form {...layout} name="basic" initialValues={{ remember: true }}>
+                <Form.Item
+                  label="Username"
+                  name="username"
+                  rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                  <Input  value={username} onChange={data => setUsername(data.target.value)}/>
+                </Form.Item>
+
+                <Form.Item
+                  label="Password"
+                  name="password"
+                  rules={[{ required: true, message: 'Please input your password!' }]}
+                >
+                  <Input value={password} onChange={data => setPasword(data.target.value)}/>
+                </Form.Item>
+                <Form.Item {...tailLayout}>
+                  <Button type="primary" htmlType="submit" onClick={handleSubmit}>
+                    Submit
+                  </Button>
+                </Form.Item>
+              </Form>
+            </Col>
+          </Row>
+        </Content>
+        <Footer>
+          Footer
+        </Footer>
+      </Layout>
     </div>
-  )
+  );
 }
 
 export default SinginView;
