fbaa3f8b4631912e352bd65c4217ede031af141d
victorfernandesraton <vfbraton@gmail.com>
2020-05-27 21:36:53 -0300

    adjust statistics

--- src/components/buckets/Bucket-utils.js
+++ src/components/buckets/Bucket-utils.js
@@ -1,1 +1,11 @@
-export const actives = (sensors) => sensors.filter(sensor => sensor.status && sensor.type=='wather-sensor' && sensor.value && sensor.value.data == true).length
+export const actives = (sensors) =>
+  sensors.filter(
+    (sensor) =>
+      sensor.status &&
+      sensor.type === "wather-sensor" &&
+      sensor.value &&
+      sensor.value.data === true
+  ).length;
+
+export const sensorVolume = (sensors) =>
+  sensors.filter((sensor) => sensor.type === "wather-sensor").length;
