4cb82f3f5a858fc2af0ba749379a1e653c235c11
victorfernandesraton <vfbraton@gmail.com>
2020-06-21 21:02:09 -0300

    adjust for use device to description

--- src/components/senseors/Sensor-utils.js
+++ src/components/senseors/Sensor-utils.js
@@ -1,9 +1,15 @@
 export const mockSensorsForTarget = (sensors) => {
   return sensors.map((sensor, key) => {
-    return {
+    let newSensor = {
       ...sensor,
       key: sensor._id.toString(),
       title: sensor.name
     }
+
+    if (sensor.device_parent && sensor.device_parent.name) {
+      newSensor.description = sensor.device_parent.name
+    }
+
+    return newSensor
   })
 }
